feeeee7e6db245fe3ba8cc9f600548cb309a449f
nelliesnoodles <sarahssmiling2@gmail.com>
2020-01-09 18:12:02 +0000

    first commit gecko one testing

---
+++ GeckoOneHome/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def load_home(request):
+    name1="Sophia"
+    name2="Jordon"
+    name3=". Dj ."
+    name4="Nellie"
+    return render(request, 'GeckoOneHome/home.html', {'chingu1':name1, 'chingu2':name2, 'chingu3':name3, 'chingu4':name4})
+
+
