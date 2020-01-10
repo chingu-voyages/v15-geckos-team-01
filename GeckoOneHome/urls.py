751427af31c3e0676df684a57f5eba84f362c088
nelliesnoodles <sarahssmiling2@gmail.com>
2020-01-10 14:39:20 +0000

    final fixes before base push

--- GeckoOneHome/urls.py
+++ GeckoOneHome/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.load_home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('success', views.load_loggedIn, name='success'),
+    path('signup', views.SignUp.as_view(), name='signup'),
 ]
--- GeckoOneHome/views.py
+++ GeckoOneHome/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+#from django import forms
+from django.contrib.auth.forms import UserCreationForm
+#from .custom_form import CustomUserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.shortcuts import redirect
 
-# Create your views here.
+
+class SignUp(generic.CreateView):
+    #form_class = CustomUserCreationForm()
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
 
 
 def load_home(request):
@@ -10,6 +23,3 @@ def load_home(request):
     name4="Nellie"
     return render(request, 'GeckoOneHome/home.html', {'chingu1':name1, 'chingu2':name2, 'chingu3':name3, 'chingu4':name4})
 
-
-def load_loggedIn(request):
-    return render(request, 'loggedIn/loggedIn.html')
