5d12ccd36e81c876c3396b4edd0f477646c2f1d9
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2020-01-12 20:00:32 -0500

    testing Django paths to js directory

--- static/js/test_js.js
+++ static/js/test_js.js
@@ -1,1 +1,11 @@
 //testing JavaScript routing in Django
+let test_item;
+
+function set_DOM() {
+  test_item = document.getElementById('test_js');
+  test_item.innerHTML = "Django path to JS file successful.";
+}
+
+window.onload = function load() {
+  set_DOM();
+}
