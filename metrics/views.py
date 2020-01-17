0e18a342d1225ea747022fd4289bb09ccbdccca5
nelliesnoodles <sarahssmiling2@gmail.com>
2020-01-17 02:11:20 +0000

    add model for goals list in metrics page

--- metrics/views.py
+++ metrics/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.shortcuts import redirect
 from django.contrib.auth import login, authenticate
+from .models import Goal
 
 
 def load_metrics(request):
-    return render(request, 'metrics/metrics.html')
+    goals_list = Goal.objects.order_by('id')
+    user_metrics = {'goals_list' : goals_list}
+    return render(request, 'metrics/metrics.html', user_metrics)
