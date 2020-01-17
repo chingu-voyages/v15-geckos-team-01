e1f072862f7527072831b45fc486d2a927eba02f
nelliesnoodles <sarahssmiling2@gmail.com>
2020-01-17 22:24:30 +0000

    added current front-end files for home/index and styling

--- GeckoOneHome/views.py
+++ GeckoOneHome/views.py
@@ -27,9 +27,6 @@ def signup(request):
 
 
 def load_home(request):
-    name1="Sophia"
-    name2="Jordon"
-    name3=". Dj ."
-    name4="Nellie"
-    return render(request, 'GeckoOneHome/home.html', {'chingu1':name1, 'chingu2':name2, 'chingu3':name3, 'chingu4':name4})
+
+    return render(request, 'GeckoOneHome/index.html')
 
