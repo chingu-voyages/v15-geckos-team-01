0af9864317418bec5feab2287450a184a04a99e5
nelliesnoodles <sarahssmiling2@gmail.com>
2020-01-30 23:22:05 +0000

    added Jordan's styles to the Sign Up page.

--- GeckoOneHome/urls.py
+++ GeckoOneHome/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.load_home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change-password.html'), name='change-password'),
     path('signup', views.signup, name='signup'),
     path('', include('metrics.urls'), name='metrics'),
 ]
+
+#path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change-password.html'), name='change-password'),
